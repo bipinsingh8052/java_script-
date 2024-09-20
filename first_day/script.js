b3a8b4ed7c458fe0b7e742cd069423030208f552
Bipin Singh <bipinsingh8052@gmail.com>
2024-09-20 12:14:23 +0530

    second

---
+++ first_day/script.js
@@ -0,0 +1,14 @@
+function pra(){
+    let a =parseInt(window.prompt("Enter Your salary"));
+    if(a<350000){
+        window.alert("Less than 3.5 lakh you dont want to pay tax");
+    }
+    else if(a>350000&&a<800000){
+        var b =a/100*10;
+        console.log(b);
+    }
+    else{
+        console.log(a/100*17);
+    }
+}
+pra();
