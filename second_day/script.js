979c48dc0a71bff9215008f04dd3c71e195debc3
Bipin Singh <bipinsingh8052@gmail.com>
2024-09-24 11:45:03 +0530

    data type

---
+++ second_day/script.js
@@ -0,0 +1,25 @@
+// let a = document.querySelector(".conatiner");
+// let b = document.querySelector('.heading');
+// console.log(a,b);
+// document.alert(a,b);
+function dom()
+{
+    let c= document.querySelector(".conatiner");
+    let d = document.querySelector('.heading');
+    console.log(c,d);
+    // d.innerHTML'
+    // <h1>helloworlld</h1>';
+    // d.style.document'
+    // background:blue';
+    d.style.color="red";
+    c.style.backgroundColor="yellow",color="orange";
+    d.innerHTML='<h1>hello world</h1>';
+    d.style.fontSize="20px";
+
+}
+
+function img()
+{
+    let imgage =querySelector('.img_section');
+    console.log(imgage);
+}
---
+++ thirdd_day/script.js
@@ -0,0 +1,7 @@
+let a=0.1;
+let b=0.4;
+
+console.log(a+b);
+let c;
+let d;
+console.log(c+d);
