06f8605270431bf0aa7f80192286ea0fc378635c
Bipin Singh <bipinsingh8052@gmail.com>
2024-09-24 12:18:34 +0530

    background image

--- thirdd_day/script.js
+++ thirdd_day/script.js
@@ -5,3 +5,27 @@ console.log(a+b);
 let c;
 let d;
 console.log(c+d);
+let click =document.querySelector("button");
+let body =document.querySelector("body");
+console.log(body);
+console.log(click);
+let count=0;
+click.addEventListener("click",function(e){
+   
+    if(count == 1)
+    {
+        body.style.backgroundImage="url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTWm8CIjAjWcm0_0_zBZ91BU5T3hIMk6h5ag&s')";
+        console.log("bipin");
+        count=count-1;
+    }
+    else{
+        body.style.backgroundImage="url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMOqNk18sY7OPjOslCJp7s_gwH-YLrg2-Wkw&s')";
+        console.log("bipin");
+        count++;
+    }
+    
+})
+
+// click.addEventListener("doubleClick",function(e){
+
+// })
