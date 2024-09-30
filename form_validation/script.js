a5b12b0c5af86bdb1a8ca37f90bf7d1060f9d205
Bipin Singh <bipinsingh8052@gmail.com>
2024-09-30 12:41:07 +0530

    normal

--- form_validation/script.js
+++ form_validation/script.js
@@ -6,8 +6,8 @@ function data(){
     let num =document.querySelector("#num");
     let black =document.querySelector("#black");
     let pass =document.querySelector("#password");
-    black.value=input.value+num.value;
-    console.log(input.value);
+    // black.value=input.value+num.value;
+    // console.log(input.value);
 
     console.log(pass.value)
 
@@ -15,6 +15,36 @@ function data(){
     
     if(input.value == ""){
         let show=document.querySelector(".input");
+        show.innerHTML = "your con't inter the name";
+        show.style.color="black";
+        input.style.border="2px solid red";
+        input.style.outlineColor="red";
+        console.log(show);
+            // window.alert("Enter the name:");
+            return false;
+    }
+    else if(num.value == ""){
+        let show=document.querySelector(".passworderr");
+        show.innerHTML = "your con't inter the name";
+        show.style.color="black";
+        input.style.border="2px solid red";
+        input.style.outlineColor="red";
+        console.log(show);
+            // window.alert("Enter the name:");
+            return false;
+    }
+    else if(pass.value == ""){
+        let show=document.querySelector(".numerror");
+        show.innerHTML = "your con't inter the name";
+        show.style.color="black";
+        input.style.border="2px solid red";
+        input.style.outlineColor="red";
+        console.log(show);
+            // window.alert("Enter the name:");
+            return false;
+    }
+    else if(black.value == ""){
+        let show=document.querySelector(".lasterror");
         show.innerHTML = "your con't inter the name";
         show.style.color="black";
         input.style.border="2px solid red";
@@ -44,11 +74,47 @@ function data(){
 // }else{
 //     show.style.display="none";
 // }
-function b(){
-    let name=querySelector("#text");
-    let erroeinput=querySelector(".input");
+function b(b,a){
+    let name = document.querySelector("#a");
+    console.log(name);
+    
+    let erroeinput = document.querySelector(".b");
+    console.log(erroeinput);
     name.style.border="2px solid black";
     name.style.outlineColor="black";
-    erroeinput.style.display="none"
+    // erroeinput.style.display="none";
+    erroeinput.innerHTML="";
+    console.log(name);
     console.log(erroeinput);
 }
+// function o(){
+//     let name = document.querySelector("#num");
+//     let erroeinput = document.querySelector(".passworderr");
+//     name.style.border="2px solid black";
+//     name.style.outlineColor="black";
+//     // erroeinput.style.display="none";
+//     erroeinput.innerHTML="";
+//     console.log(name);
+//     console.log(erroeinput);
+// }
+// function n(){
+//     let name = document.querySelector("#password");
+//     let erroeinput = document.querySelector(".numerror");
+//     name.style.border="2px solid black";
+//     name.style.outlineColor="black";
+//     // erroeinput.style.display="none";
+//     erroeinput.innerHTML="";
+//     console.log(name);
+//     console.log(erroeinput);
+// }
+
+// function m(){
+//     let name = document.querySelector("#black");
+//     let erroeinput = document.querySelector(".lasterror");
+//     name.style.border="2px solid black";
+//     name.style.outlineColor="black";
+//     // erroeinput.style.display="none";
+//     erroeinput.innerHTML="";
+//     console.log(name);
+//     console.log(erroeinput);
+// }
