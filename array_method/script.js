920b7f973a64d12c90e83c388bbefd02601a0bfd
Bipin Singh <bipinsingh8052@gmail.com>
2024-10-22 11:32:53 +0530

    first

--- array_method/script.js
+++ array_method/script.js
@@ -125,3 +125,41 @@ function run(){
     ).join(" ");
     console.log(normal); 
 }
+
+
+
+
+
+
+let r = "hellow"
+r.charAt(3);
+console.log(r);
+r.charCodeAt(3);
+console.log(r);
+
+let n = Math.floor(4.5);
+console.log(n);
+
+let m = Math.ceil(4.5);
+console.log(m);
+
+let a = Math.PI;
+console.log(a);
+
+let b = Math.sqrt();
+console.log(b);
+
+let c = Math.sqrt(1/2);
+console.log(c);
+
+let d = Math.pow(4,2);
+console.log(d);
+
+let e = Math.round(4.5);
+console.log(e);
+
+let str = "we are developers";
+let len = str.length;
+let f = Math.random();
+let ans = len*f;
+console.log(ans);
