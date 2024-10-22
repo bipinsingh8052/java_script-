d5d88e2232edcf5948e55e333bb423eb75131e67
Bipin Singh <bipinsingh8052@gmail.com>
2024-10-22 12:17:03 +0530

    normal

---
+++ object_method/script.js
@@ -0,0 +1,104 @@
+console.log("hello");
+let cart_info=[
+
+    {
+    "id":1,
+    "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgPbd2MBbw3o5_yzYC_pPjoVNKUx7WCrMN3g&s",
+    "pname":"nike",
+    "bname":"shoes",
+    "price":"₹245",
+    },
+
+    {
+        "id":1,
+        "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgPbd2MBbw3o5_yzYC_pPjoVNKUx7WCrMN3g&s",
+        "pname":"nike",
+        "bname":"shoes",
+        "price":"₹245",
+        },
+        {
+            "id":1,
+            "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgPbd2MBbw3o5_yzYC_pPjoVNKUx7WCrMN3g&s",
+            "pname":"nike",
+            "bname":"shoes",
+            "price":"₹245",
+    
+        },    
+        {
+            "id":1,
+            "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgPbd2MBbw3o5_yzYC_pPjoVNKUx7WCrMN3g&s",
+            "pname":"nike",
+            "bname":"shoes",
+          "price":"₹245",
+            }, {
+                "id":1,
+                "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgPbd2MBbw3o5_yzYC_pPjoVNKUx7WCrMN3g&s",
+                "pname":"nike",
+                "bname":"shoes",
+               "price":"₹245",
+                }, {
+                    "id":1,
+                    "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgPbd2MBbw3o5_yzYC_pPjoVNKUx7WCrMN3g&s",
+                    "pname":"nike",
+                    "bname":"shoes",
+                    "price":"₹245",
+                    }, {
+                        "id":1,
+                        "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgPbd2MBbw3o5_yzYC_pPjoVNKUx7WCrMN3g&s",
+                        "pname":"nike",
+                        "bname":"shoes",
+                        "price":"₹245",
+                        }, {
+                            "id":1,
+                            "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgPbd2MBbw3o5_yzYC_pPjoVNKUx7WCrMN3g&s",
+                            "pname":"nike",
+                            "bname":"shoes",
+                            "price":"₹245",
+                            }, {
+                                "id":1,
+                                "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgPbd2MBbw3o5_yzYC_pPjoVNKUx7WCrMN3g&s",
+                                "pname":"nike",
+                                "bname":"shoes",
+                               "price":"₹245",
+                                }, {
+                                    "id":1,
+                                    "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgPbd2MBbw3o5_yzYC_pPjoVNKUx7WCrMN3g&s",
+                                    "pname":"nike",
+                                    "bname":"shoes",
+                                    "price":"₹245",
+                                    }, {
+                                        "id":1,
+                                        "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgPbd2MBbw3o5_yzYC_pPjoVNKUx7WCrMN3g&s",
+                                        "pname":"nike",
+                                        "bname":"shoes",
+                                       "price":"₹245",
+                                        }, {
+                                            "id":1,
+                                            "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgPbd2MBbw3o5_yzYC_pPjoVNKUx7WCrMN3g&s",
+                                            "pname":"nike",
+                                            "bname":"shoes",
+                                           "price":"₹245",
+                                            },
+
+]
+let nu =document.querySelector(".cart");
+nu.innerHTML= cart_info.map((cart)=>
+`<div class="conatiner">
+            <div class="heading">
+                <img src='${cart.image}
+                ' alt="">
+            </div>
+            <div class="body">
+              <p>${cart.bname}</p>
+              <h5>${cart.pname}</h5>
+              
+            </div>
+            <div class="footer">
+                
+                <p>
+                    ${cart.price}
+                </p>
+            </div>
+        </div>
+`
+).join(" ");
