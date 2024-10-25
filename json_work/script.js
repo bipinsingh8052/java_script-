2429a8b026aa8509d07764ac9fe0d6bd7e1676d2
Bipin Singh <bipinsingh8052@gmail.com>
2024-10-25 12:25:10 +0530

    normal

--- json_work/script.js
+++ json_work/script.js
@@ -1,12 +1,40 @@
+function del(id){
+    let yes=window.confirm("DO you delete the item");
+    if(yes){
+        fetch(`http://localhost:4000/product/${id}`,{
+            method:"DELETE"
+        });
+    }
+    else{
+        window.alert("invalid ");
+    }
+}
+function update(id){
+    let obj={
+        "image":document.querySelector("").value,
+        "pname":document.querySelector("").value,
+        "bname":document.querySelector(" ").value,
+        "price":document.querySelector("").value
+    }
+    {
+        method:"PUT",
+        body:JSON.stringify(obj)
+    }
+}
 async function run() {
     let data =await fetch('http://localhost:4000/product');
     let responise=await data.json();
     console.log(responise);
     let table =document.querySelector("table");
     table.innerHTML=responise.map((item)=>`
-    <tr>${item.id}</tr>
-<tr>${item.image}</tr>
-<tr>${item.pname}</tr>
-<tr>${item.price}</tr>
-    `)
+   <tr>
+            <td>${item.id}</td>
+            <td><img width="100px" src="${item.image}" alt="ram"></td>
+            <td>${item.pname}</td>
+            <td>${item.bname}</td>
+            <td>${item.price}</td>
+            <td><button onclick = "del(${item.id})">Delete</button></td>
+            <td><button onclick = "update(${item.id})">Update</button></td>
+        </tr>
+    `).join(" ");
 }
