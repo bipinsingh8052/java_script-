2eb812707677d2cc5598ec6aa321687ed98b15e0
Bipin Singh <bipinsingh8052@gmail.com>
2024-11-19 17:12:47 +0530

    json work and json npm and node.js

--- json_work/script2.js
+++ json_work/script2.js
@@ -1,12 +1,31 @@
-async function fet(){
-    let a =await fetch("https://jsonplaceholder.typicode.com/comments")
+// async function fet(){
+//     let a =await fetch("https://jsonplaceholder.typicode.com/comments")
+//     let b= await a.json();
+//     console.log(b);
+//     let ans =b.map((obj)=> `
+//         <h1>${obj.email}</h1>
+//         <h1>${obj.id}</h1>
+//         `
+//     ).join(" ");
+//     document.querySelector("#conatiner").innerHTML=ans;
+// }
+// fet();
+
+
+async function run(){
+    let a= await fetch ("http://localhost:3000/STUDENT");
     let b= await a.json();
-    console.log(b);
-    let ans =b.map((obj)=> `
-        <h1>${obj.email}</h1>
-        <h1>${obj.id}</h1>
-        `
-    ).join(" ");
-    document.querySelector("#conatiner").innerHTML=ans;
+    let c= b.map((v)=>{
+        return `
+        <h1>${v.id}</h1>
+        <h2>${v.name}</h2>
+        <h2>${v.age}</h2>
+        <h2>${v.address}</h2>`
+    }).join(" ");
+    document.querySelector(".card").innerHTML=c;
 }
-fet();
+run();
+
+
+
+
