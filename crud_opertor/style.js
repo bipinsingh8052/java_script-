867ce2d035f393ae2dd8757023b2537ff963f264
Bipin Singh <bipinsingh8052@gmail.com>
2024-11-20 18:57:49 +0530

    gtt

--- crud_opertor/style.js
+++ crud_opertor/style.js
@@ -1,13 +1,32 @@
+function delt(id)
+{
+    alert(id);
+    // fetch(`http://localhost:3000/STUDENT/${id}`,{
+    //     method:'DELETE'
+    // })
+    // .then(re=>{return alert("Deleted sucessfully...!!")});
+}
+
+
 async function run(){
     let a= await fetch ("http://localhost:3000/STUDENT");
     let b= await a.json();
     let c= b.map((v)=>{
         return `
-        <h1>${v.id}</h1>
-        <h2>${v.name}</h2>
-        <h2>${v.age}</h2>
-        <h2>${v.address}</h2>`
+        <tr>
+        <td>${v.id}</td>
+        <td>${v.name}</td>
+        <td>${v.age}</td>
+        <td>${v.address}</td>
+    <td><button onclick= "  delt('${v.id}')">delete </button></td>
+    <td><button>update</button></td>
+        </tr>
+        `
     }).join(" ");
-    document.querySelector(".card").innerHTML=c;
+     document.querySelector("#card").innerHTML=c;
+    
 }
+
+
+
 run();
