f1c76e3582d42fcdf04f3891eae7bf953bbfa5e5
Bipin Singh <bipinsingh8052@gmail.com>
2024-11-21 12:34:21 +0530

    add method and update method

--- crud_opertor/style.js
+++ crud_opertor/style.js
@@ -1,6 +1,6 @@
 function del(id)
 {
-    alert(id);
+ 
     fetch(`http://localhost:3000/STUDENT/${id}`,{
         method:'DELETE'
     })
@@ -14,19 +14,38 @@ async function run(){
     let c= b.map((v)=>{
         return `
         <tr>
-        <td>${v.id}</td>
-        <td>${v.name}</td>
-        <td>${v.age}</td>
-        <td>${v.address}</td>
-    <td><button onclick = "del('${v.id}')">delete </button></td>
-    <td><button>update</button></td>
+            <td>${v.id}</td>
+            <td>${v.name}</td>
+            <td>${v.age}</td>
+            <td>${v.address}</td>
+            <td><button onclick = "del('${v.id}')">delete </button></td>
+            <td><button>update</button></td>
         </tr>
         `
     }).join(" ");
-     document.querySelector("#card").innerHTML=c;
-    
+     document.querySelector("#card").innerHTML=c;  
 }
 
 
 
 run();
+
+
+function Insertdata(){
+    let data ={
+        name : document.querySelector("#name").value,
+        age : document.querySelector("#age").value,
+        address : document.querySelector("#address").value
+    }
+    console.log(data.name);
+    console.log(data.age);
+    console.log(data.address);
+    fetch("http://localhost:3000/STUDENT",{
+        method:"POST",
+        headers:{
+            'Content_type': 'application/json',
+        },
+        body:JSON.stringify(data)
+    })
+    .then(rev=>alert("INserting data!!!!!!"));
+}
